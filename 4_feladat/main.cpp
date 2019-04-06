eb5ee1afd8b0364fed0c7ef08736a0a848bbf12f
szm1000 <48022212+szm1000@users.noreply.github.com>
2019-04-06 20:10:52 +0200

    Update main.cpp

--- 4_feladat/main.cpp
+++ 4_feladat/main.cpp
@@ -12,24 +12,24 @@ bool dbeq(double a, double b, double eps){
 }
 
 int main(){
-    cout.precision(7);
-    vec2<double> const v1 = {1.0, 2.0};
+    cout.precision(12);
+    vec2<double> const v1 = {1.2, 2.3};
     vec2<double> const v2 = {987.654, 123.456};
     vec2<double> v3 = v1;
     vec2<double> v4 = v2;
     double const epsilon = 1e-10;
 
-    vector<vec2<double>> sol_v= {{988.654, 125.456}, {-986.654, -121.456}, {2962.962, 370.368}, {2962.962, 370.368},
-    {4444.443, 555.552}, {1.0, 0.1249992406247532030},{0.9922779694532460, 0.12403399267032780}};
-    vector<vec2<double>> test_v = {v1 + v2, v1 - v2, v2 * 3, 3 * v2, 4.5 * v2, v2 / 987.654, normalize(v2)};
+    vector<vec2<double>> sol_v= {{988.854, 125.756}, {-986.454, -121.156}, {2962.962, 370.368}, {2962.962, 370.368},
+    {4444.443, 555.552}, {1.00000405002, 0.124999746874},{0.9922779694532460, 0.12403399267032780}};
+    vector<vec2<double>> test_v = {v1 + v2, v1 - v2, v2 * 3, 3 * v2, 4.5 * v2, v2 / 987.65, normalize(v2)};
 
     for(int i = 0; i < static_cast<int>(sol_v.size()); i++){
-        if(!(sol_v[i] == test_v[i])){
+        if(!vec2eq(sol_v[i], test_v[i], epsilon)){
             cout<<"Error! vector["<< i << "]" <<endl;
         }
     }
 
-    vector<double> sol_v2 = {1234.566, 2.23606797749978969, 5};
+    vector<double> sol_v2 = {1469.1336, 2.59422435421, 6.73};
     vector<double> test_v2 = {dot(v1,v2), length(v1), sqlength(v1)};
 
     for(int i = 0; i < static_cast<int>(sol_v2.size()); i++){
@@ -39,27 +39,27 @@ int main(){
     }
 
     v3 += v4;
-    vec2<double> const sol1 = {988.654, 125.456};
-    if(v3 != sol1){
+    vec2<double> const sol1 = {988.854, 125.756};
+    if(!vec2eq(v3, sol1, epsilon)){
         cout<<"Error in += operator"<<endl;
     }
 
     v3 -= v4;
-    vec2<double> const sol2 = {1.0, 2.0};
-    if(v3 != sol2){
+    vec2<double> const sol2 = {1.2, 2.3};
+    if(!vec2eq(v3, sol2, epsilon)){
         cout<<"Error in -= operator"<<endl;
     }
 
     v3 = v1;
     v3 *= 2;
-    vec2<double> const sol3 = {2.0, 4.0};
-    if(v3 != sol3){
+    vec2<double> const sol3 = {2.4, 4.6};
+    if(!vec2eq(v3, sol3, epsilon)){
         cout<<"Error in *= operator"<<endl;
     }
 
     v3 /= 2;
-    vec2<double> const sol4 = {1.0, 2.0};
-    if(v3 != sol4){
+    vec2<double> const sol4 = {1.2, 2.3};
+    if(!vec2eq(v3, sol4, epsilon)){
         cout<<"Error in /= operator"<<endl;
     }
 
