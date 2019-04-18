8a182d8c9e7049a98b3cb0c5f35bc9b1c89c42b8
szm1000 <48022212+szm1000@users.noreply.github.com>
2019-04-18 23:22:32 +0200

    Update main.cpp

--- 5_feladat/main.cpp
+++ 5_feladat/main.cpp
@@ -6,12 +6,15 @@ using namespace std;
 
 int main(){
     cout.precision(7);
-    sq_matrix<double> const m1 = {821,13,463,215,992,708,880,12,974,79,564,829,95,568,63,149};
-    sq_matrix<double> const m2 = {567,573,840,105,223,289,169,928,831,788,661,261,59,809,449,49};
+    sq_matrix<double> const m1({821,13,463,215,992,708,880,12,974,79,564,829,95,568,63,149}, 4);
+    sq_matrix<double> const m2({567,573,840,105,223,289,169,928,831,788,661,261,59,809,449,49}, 4);
     sq_matrix<double> m3 = m2;
     double eps = 1e-10;
-    cout<<m1<<endl;
+
     //Correct values from https://matrix.reshish.com
+
+    cout<<m1<<endl;
+
     if(!sq_matrix_eq(m1, m1, eps)){
         cout<<"Error in equality check function!"<<endl;
     }    
@@ -20,14 +23,14 @@ int main(){
     } 
 
     sq_matrix<double> a1 = m1 + m2;
-    sq_matrix<double> const add_res = {1388,586,1303,320,1215,997,1049,940,1805,867,1225,1090,154,1377,512,198};
+    sq_matrix<double> const add_res({1388,586,1303,320,1215,997,1049,940,1805,867,1225,1090,154,1377,512,198}, 4);
     if(!sq_matrix_eq(a1, add_res, eps)){
         cout<<"Addition operator error!"<<endl;
         cout<<a1<<endl;
         cout<<add_res<<endl;
     }
     sq_matrix<double> a2 = m1 - m2;
-    sq_matrix<double> const sub_res  = {254,-560,-377,110,769,419,711,-916,143,-709,-97,568,36,-241,-386,100};
+    sq_matrix<double> const sub_res({254,-560,-377,110,769,419,711,-916,143,-709,-97,568,36,-241,-386,100},4);
     if(!sq_matrix_eq(a2, sub_res, eps)){
         cout<<"Substraction operator error!"<<endl;
         cout<<a2<<endl;
@@ -35,7 +38,7 @@ int main(){
    }
 
     sq_matrix<double> a3 = m1 * m2;
-    sq_matrix<double> const prod_res1  = {865844,1012969,1094415,229647,1452336,1476176,1540000,991452,1087470,1696026,1576536,363407,241673,388772,284336,560823};
+    sq_matrix<double> const prod_res1({865844,1012969,1094415,229647,1452336,1476176,1540000,991452,1087470,1696026,1576536,363407,241673,388772,284336,560823}, 4);
     if(!sq_matrix_eq(a3, prod_res1, eps)){
         cout<<"Matrix product1 error!";
         cout<<a3<<endl;
@@ -43,7 +46,7 @@ int main(){
     }
 
     sq_matrix<double> a4 = m2 * m1;
-    sq_matrix<double> const prod_res2  = {1862058, 539055, 1247136, 840786, 722537, 747966, 511349, 329786, 2132556, 769174, 1467440, 774979, 1292948, 636842, 995560, 401915};
+    sq_matrix<double> const prod_res2({1862058, 539055, 1247136, 840786, 722537, 747966, 511349, 329786, 2132556, 769174, 1467440, 774979, 1292948, 636842, 995560, 401915}, 4);
         if(!sq_matrix_eq(a4, prod_res2, eps)){
         cout<<"Matrix product2 error!"<<endl;
         cout<<a4<<endl;
@@ -51,7 +54,7 @@ int main(){
     }
 
     sq_matrix<double> a5 = m2 * 2.2;
-    sq_matrix<double> const mul1  = {1247.4, 1260.6, 1848, 231, 490.6, 635.8, 371.8, 2041.6, 1828.2, 1733.6, 1454.2, 574.2, 129.8, 1779.8, 987.8, 107.8};
+    sq_matrix<double> const mul1({1247.4, 1260.6, 1848, 231, 490.6, 635.8, 371.8, 2041.6, 1828.2, 1733.6, 1454.2, 574.2, 129.8, 1779.8, 987.8, 107.8}, 4);
     if(!sq_matrix_eq(a5, mul1, eps)){
         cout<<"Matrix multiplication1 error!"<<endl;
         cout<<a5<<endl;
@@ -66,7 +69,7 @@ int main(){
     }
 
     sq_matrix<double> a6d = m2/100;    
-    sq_matrix<double> div = {5.67, 5.73, 8.4, 1.05, 2.23, 2.89, 1.69, 9.28, 8.31, 7.88, 6.61, 2.61, 0.59, 8.09, 4.49, 0.49};
+    sq_matrix<double> div({5.67, 5.73, 8.4, 1.05, 2.23, 2.89, 1.69, 9.28, 8.31, 7.88, 6.61, 2.61, 0.59, 8.09, 4.49, 0.49}, 4);
     if(!sq_matrix_eq(a6d, div, eps)){
         cout<<"Matrix division error!"<<endl;
         cout<<a6d<<endl;
@@ -150,6 +153,47 @@ int main(){
         cout<<prod_res1<<endl;
     }
 
+    sq_matrix<double> m19 = m2;
+    m19 *= 2.2;
+    if(!sq_matrix_eq(m19, mul1, eps)){
+        cout<<"Matrix multiplication error! (*=)"<<endl;
+        cout<<m19<<endl;
+        cout<<mul1<<endl;
+    }
+
+    sq_matrix<double> m20 = m2;
+    m20 /= 100;
+    if(!sq_matrix_eq(m20, div, eps)){
+        cout<<"Matrix division error! (/=)"<<endl;
+        cout<<m20<<endl;
+        cout<<div<<endl;
+    }
+
+    sq_matrix<double> m21 = m1;
+    m21 += m2;
+    if(!sq_matrix_eq(m21, add_res, eps)){
+        cout<<"Matrix addition error! (+=)"<<endl;
+        cout<<m21<<endl;
+        cout<<add_res<<endl;
+    }
+
+    sq_matrix<double> m22 = m1;
+    m22 -= m2;
+    if(!sq_matrix_eq(m22, sub_res, eps)){
+        cout<<"Matrix substraction error! (-=)"<<endl;
+        cout<<m22<<endl;
+        cout<<sub_res<<endl;
+    }
+
+    sq_matrix<double> m23 = m1;
+    m23 *= m2;
+    if(!sq_matrix_eq(m23, prod_res1, eps)){
+        cout<<"Matrix substraction error! (-=)"<<endl;
+        cout<<m23<<endl;
+        cout<<prod_res1<<endl;
+    }
+
     return 0;
 
 }
+
